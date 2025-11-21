d2254639e1f92f2eb543411148b47662a6723de8
Achilovraf <Achilovraf@gmail.com>
2025-11-21 15:24:32 +0500

    Add environment-based base path for dev and production

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [vue()],
-  base: "/albadent/",
+  base: process.env.NODE_ENV === 'production' ? '/albadent/' : '/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
@@ -20,4 +20,3 @@ export default defineConfig({
     ],
   },
 });
- 
